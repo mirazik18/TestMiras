8c23b514655aaa18badda44799c389b8e0922d3b
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-08-29 21:52:08 +0600

    C+-+

---
+++ Rok/FirstApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(10, 15);
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Beep();
+            Console.WriteLine("Введите длину ноги ");
+          string name =  Console.ReadLine();
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}
