c3d88ad76f392307425b45789a7e2008c7726de8
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-10-31 22:32:14 +0600

    27.09

---
+++ 27.09/HWModul8/Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWModul8
+{
+    public class Block
+    {
+        static public Block[] block = new Block[10];
+        public int num = new int();
+        public Block this[int index]
+        {
+            get { return block[index]; }    
+            set { block[index] = new Block(value.num * value.num); }
+        }
+        public Block(int num)
+        {
+            this.num = num;
+        }
+    }
+}
---
+++ 27.09/HWModul8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWModul8
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Block.block[0] = new Block(2);
+            Console.WriteLine(Block.block[0].num);
+        }
+    }
+}
