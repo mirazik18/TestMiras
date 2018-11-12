7aaae5af6ae3fb3583501e8eec699a286cc42668
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-11-12 17:02:52 +0300

    Add files via upload

---
+++ SERIAL/ConsoleApp9/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERIAL
+{
+    [Serializable]
+
+    public class Book
+    {
+        public string Name { get; set; }
+        public int Cost { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public Book(string name, int cost, string author, int year)
+        {
+            Name = name;
+            Cost = cost;
+            Author = author;
+            Year = year;
+        }
+    }
+    
+}
---
+++ SERIAL/ConsoleApp9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SERIAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Война и мир", 1488, "Абдишев", 1488);
+            Book book2 = new Book("Наши отцы и братья", 1337, "Мирас", 1488);
+
+            BinaryFormatter formatter = new BinaryFormatter();
+           
+
+            using (FileStream fs = new FileStream("Book.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, book1);
+                formatter.Serialize(fs, book2);
+                Console.WriteLine("Сериализован");
+            }
+
+            using (FileStream fs = new FileStream("Book.dat", FileMode.OpenOrCreate))
+            {
+                Book newBook = (Book)formatter.Deserialize(fs);
+                Console.WriteLine("Десирилизация");
+                Console.WriteLine("Название: {0} Цена: {1}  Автор: {2}  Год издания: {3}", newBook.Name, newBook.Cost, newBook.Author, newBook.Year);
+            }
+
+            Console.ReadLine();
+
+
+        
+
+
+        }
+    }
+}
