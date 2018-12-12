98d8e1b92c84da259e3e4744e5027065ee4df1f8
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-12-12 20:16:23 +0600

    Add files via upload

---
+++ STAR/ConsoleApp1/Planet.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Planet
+    {
+
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("rotation_period")]
+        public string rotation_period { get; set; }
+
+        [JsonProperty("orbital_period")]
+        public string orbital_period { get; set; }
+
+        [JsonProperty("diameter")]
+        public string diameter { get; set; }
+
+        [JsonProperty("climate")]
+        public string climate { get; set; }
+
+        [JsonProperty("gravity")]
+        public string gravity { get; set; }
+
+        [JsonProperty("terrain")]
+        public string terrain { get; set; }
+
+        [JsonProperty("surface_water")]
+        public string surface_water { get; set; }
+
+        [JsonProperty("population")]
+        public string population { get; set; }
+
+        [JsonProperty("residents")]
+        public IList<object> residents { get; set; }
+
+        [JsonProperty("films")]
+        public IList<string> films { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime created { get; set; }
+
+        [JsonProperty("edited")]
+        public DateTime edited { get; set; }
+
+        [JsonProperty("url")]
+        public string url { get; set; }
+    }
+}
---
+++ STAR/ConsoleApp1/Starship.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Starship
+    {
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("model")]
+        public string model { get; set; }
+
+        [JsonProperty("manufacturer")]
+        public string manufacturer { get; set; }
+
+        [JsonProperty("cost_in_credits")]
+        public string cost_in_credits { get; set; }
+
+        [JsonProperty("length")]
+        public string length { get; set; }
+
+        [JsonProperty("max_atmosphering_speed")]
+        public string max_atmosphering_speed { get; set; }
+
+        [JsonProperty("crew")]
+        public string crew { get; set; }
+
+        [JsonProperty("passengers")]
+        public string passengers { get; set; }
+
+        [JsonProperty("cargo_capacity")]
+        public string cargo_capacity { get; set; }
+
+        [JsonProperty("consumables")]
+        public string consumables { get; set; }
+
+        [JsonProperty("hyperdrive_rating")]
+        public string hyperdrive_rating { get; set; }
+
+        [JsonProperty("MGLT")]
+        public string MGLT { get; set; }
+
+        [JsonProperty("starship_class")]
+        public string starship_class { get; set; }
+
+        [JsonProperty("pilots")]
+        public IList<object> pilots { get; set; }
+
+        [JsonProperty("films")]
+        public IList<string> films { get; set; }
+
+        [JsonProperty("created")]
+        public DateTime created { get; set; }
+
+        [JsonProperty("edited")]
+        public DateTime edited { get; set; }
+
+        [JsonProperty("url")]
+        public string url { get; set; }
+    }
+}
