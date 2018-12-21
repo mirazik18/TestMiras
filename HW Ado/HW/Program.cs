3234fdf7de4f4e6378d48fb68b841779bac1f162
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-12-21 14:34:15 +0600

    Add files via upload

---
+++ HW Ado/HW/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection())
+            {
+               
+                connection.ConnectionString = ConfigurationManager.ConnectionStrings["DataBaseConnectionString"].ConnectionString;
+                connection.Open();
+
+                var command = new SqlCommand();
+                command.Connection = connection;
+                command.CommandText = "create table gruppa ( Id int NOT NULL, Name nvarchar(255) NOT NULL );";
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
